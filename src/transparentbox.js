1e283a5bd37055526a81c06ed591e6dd3a86b3ec
VicKowa <vkowa@gmx.de>
2021-06-28 11:16:21 +0200

    first commit

---
+++ src/transparentbox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function transparentbox() {
+    return (
+        <div>
+            <div className="app">
+                 <TodoList />
+            </div>
+        </div>
+    )
+}
