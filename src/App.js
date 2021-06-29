ebcc71a4aae436f9e936e8b4b07a2c03e36e57cf
VicKowa <vkowa@gmx.de>
2021-06-29 13:21:09 +0200

    node

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import TodoList from './components/TodoList';
+
+
+
+function App() {
+  
+  return (
+    <div class = "box">
+      <div className="app">
+                 <TodoList />
+            </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import TodoForm from './TodoForm';
+import Todo from './Todo';
+import APIHelper from '../APIHelper';
+function TodoList() {
+  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState("")
+   
+  useEffect(() => {
+    const fetchTodoAndSetTodos = async () => {
+      const todos = await APIHelper.getAllTodos()
+      setTodos(todos)
+    }
+    fetchTodoAndSetTodos()
+  }, [])
+
+  const addTodo = async e => {
+    e.preventDefault()
+    if (!todo) {
+      alert("please enter something")
+      return
+    }
+    if (todos.some(({ task }) => task === todo)) {
+      alert(`Task: ${todo} already exists`)
+      return
+    }
+    const newTodo = await APIHelper.createTodo(todo)
+    setTodos([...todos, newTodo])
+  }
+
+  const updateTodo = async (e, id) => {
+    e.stopPropagation()
+    const payload = {
+      completed: !todos.find(todo => todo._id === id).completed,
+    }
+    const updatedTodo = await APIHelper.updateTodo(id, payload)
+    setTodos(todos.map(todo => (todo._id === id ? updatedTodo : todo)))
+  }
+
+  const removeTodo = async (e, id) => {
+    try {
+      e.stopPropagation()
+      await APIHelper.deleteTodo(id)
+      setTodos(todos.filter(({ _id: i }) => id !== i))
+    } catch (err) {}
+  }
+ 
+   
+ /* const completeTodo = id => {
+    let updatedTodos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };*/
+
+
+     
+      return (
+        <>
+          <h1>What's the Plan for Today?</h1>
+          <TodoForm onSubmit={addTodo} />
+          <Todo
+            todos={todos}
+            //completeTodo={completeTodo}
+            removeTodo={removeTodo}
+            updateTodo={updateTodo}
+          />
+        </>
+      );
+    }
+
+export default TodoList
